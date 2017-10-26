2850d71f2bb78d2d628940a3da5a052312c441dd
CHIA-YU-TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-26 17:52:16 +0800

    練習三：設定Route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home',function(){
 Route::get('home',function(){
     return view('home.index');
 });
+
+Route::get('hello/{name?}', ['as' => 'hello.index', 'uses' => 'HelloController@index']);
