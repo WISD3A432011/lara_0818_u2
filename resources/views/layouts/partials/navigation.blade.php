00fc80fe7544437cce6b46c6c44fca5d99949345
CHIA-YU-TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-10-26 17:44:51 +0800

    練習二：編輯navigation.blade.php

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,7 +1,8 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/26
- * Time: 下午 05:42
- */
+<ul>
+    <li>
+        <a href="#">Item1</a>
+    </li>
+    <li>
+        <a href="#">Item2</a>
+    </li>
+</ul>
